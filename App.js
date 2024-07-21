9825f5e1fd865f7ffa005433a67abe50206b11cb
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2024-07-21 13:54:23 +0530

    styling is completed as per design

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Task from './components/Task';
 export default function App() {
   return (
     <View style={styles.container}>
       {/* todo tasks  */}
-      <View>
-        <Text >
-          Where are you interest I want you by my side
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>
+          Todays Task's
         </Text>
+        {/* all the tasks */}
+        <View style={styles.items}>
+          <Task text="text 1" />
+          <Task text="text 2" />
+
+        </View>
+
       </View>
     </View>
   );
@@ -17,8 +24,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  taskWrapper: {
+    padding: 80,
+    paddingHorizontal: 20,
+
+  }
+  ,
+  items: {
+    marginTop: 30
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+
 });
